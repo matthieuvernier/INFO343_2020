1fbc45fc6d8cd9046987db05b510e8f3e6b80cd8
mvernier <mvernier@inf.uach.cl>
2020-07-03 15:40:28 -0400

    challenge nlp

---
+++ challenge_nlp/TextClassification-CNN.py
@@ -0,0 +1,333 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import torch
+from torchtext import data
+from torchtext import datasets
+import spacy
+
+
+# In[2]:
+
+
+TEXT = data.Field(tokenize='spacy', batch_first = True)
+TOXIC = data.LabelField(dtype = torch.float)
+
+
+# In[3]:
+
+
+fields = [(None, None),('comment_text', TEXT), ('toxic', TOXIC),(None, None),(None, None),(None, None),(None, None),(None, None)]
+
+
+# In[4]:
+
+
+import random
+import numpy as np
+
+SEED = 1234
+
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+
+train_data, test_data = data.TabularDataset.splits(
+                                        path = 'data',
+                                        train = 'train.csv',
+                                        test = 'test.csv',
+                                        format = 'csv',
+                                        fields = fields,
+                                        skip_header = True
+)
+
+train_data, valid_data = train_data.split(random_state = random.seed(SEED))
+
+
+# In[5]:
+
+
+MAX_VOCAB_SIZE = 25_000
+
+TEXT.build_vocab(train_data, 
+                 max_size = MAX_VOCAB_SIZE, 
+                 vectors = "glove.6B.100d", 
+                 unk_init = torch.Tensor.normal_)
+
+TOXIC.build_vocab(train_data)
+
+
+# In[23]:
+
+
+BATCH_SIZE = 64
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_iterator, valid_iterator, test_iterator = data.BucketIterator.splits(
+    (train_data, valid_data, test_data), 
+    batch_size = BATCH_SIZE, 
+    device = device)
+
+
+# In[7]:
+
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNN1d(nn.Module):
+    def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, output_dim, 
+                 dropout, pad_idx):
+        
+        super().__init__()
+        
+        self.embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx = pad_idx)
+        
+        self.convs = nn.ModuleList([
+                                    nn.Conv1d(in_channels = embedding_dim, 
+                                              out_channels = n_filters, 
+                                              kernel_size = fs)
+                                    for fs in filter_sizes
+                                    ])
+        
+        self.fc = nn.Linear(len(filter_sizes) * n_filters, output_dim)
+        
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, text):
+        
+        #text = [batch size, sent len]
+        
+        embedded = self.embedding(text)
+                
+        #embedded = [batch size, sent len, emb dim]
+        
+        embedded = embedded.permute(0, 2, 1)
+        
+        #embedded = [batch size, emb dim, sent len]
+        
+        conved = [F.relu(conv(embedded)) for conv in self.convs]
+            
+        #conved_n = [batch size, n_filters, sent len - filter_sizes[n] + 1]
+        
+        pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]
+        
+        #pooled_n = [batch size, n_filters]
+        
+        cat = self.dropout(torch.cat(pooled, dim = 1))
+        
+        #cat = [batch size, n_filters * len(filter_sizes)]
+            
+        return self.fc(cat)
+
+
+# In[8]:
+
+
+INPUT_DIM = len(TEXT.vocab)
+EMBEDDING_DIM = 100
+N_FILTERS = 100
+FILTER_SIZES = [3,4,5]
+OUTPUT_DIM = 1
+DROPOUT = 0.5
+PAD_IDX = TEXT.vocab.stoi[TEXT.pad_token]
+
+model = CNN1d(INPUT_DIM, EMBEDDING_DIM, N_FILTERS, FILTER_SIZES, OUTPUT_DIM, DROPOUT, PAD_IDX)
+
+
+# In[9]:
+
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+print(f'The model has {count_parameters(model):,} trainable parameters')
+
+
+# In[10]:
+
+
+pretrained_embeddings = TEXT.vocab.vectors
+
+model.embedding.weight.data.copy_(pretrained_embeddings)
+
+
+# In[11]:
+
+
+UNK_IDX = TEXT.vocab.stoi[TEXT.unk_token]
+
+model.embedding.weight.data[UNK_IDX] = torch.zeros(EMBEDDING_DIM)
+model.embedding.weight.data[PAD_IDX] = torch.zeros(EMBEDDING_DIM)
+
+
+# In[12]:
+
+
+import torch.optim as optim
+
+optimizer = optim.Adam(model.parameters())
+
+criterion = nn.BCEWithLogitsLoss()
+
+model = model.to(device)
+criterion = criterion.to(device)
+
+
+# In[13]:
+
+
+def binary_accuracy(preds, y):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8, NOT 8
+    """
+
+    #round predictions to the closest integer
+    rounded_preds = torch.round(torch.sigmoid(preds))
+    correct = (rounded_preds == y).float() #convert into float for division 
+    acc = correct.sum() / len(correct)
+    return acc
+
+
+# In[31]:
+
+
+def train(model, iterator, optimizer, criterion):
+    
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.train()
+    
+    for batch in iterator:
+        
+        optimizer.zero_grad()
+        
+        predictions = model(batch.comment_text).squeeze(1)
+        
+        loss = criterion(predictions, batch.toxic)
+        
+        acc = binary_accuracy(predictions, batch.toxic)
+        
+        loss.backward()
+        
+        optimizer.step()
+        
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+# In[32]:
+
+
+def evaluate(model, iterator, criterion):
+    
+    epoch_loss = 0
+    epoch_acc = 0
+    
+    model.eval()
+    
+    with torch.no_grad():
+    
+        for batch in iterator:
+           
+            predictions = model(batch.comment_text).squeeze(1)
+            
+            loss = criterion(predictions, batch.toxic)
+            
+            acc = binary_accuracy(predictions, batch.toxic)
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+        
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+# In[33]:
+
+
+import time
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
+
+# In[ ]:
+
+
+N_EPOCHS = 5
+
+best_valid_loss = float('inf')
+
+for epoch in range(N_EPOCHS):
+
+    start_time = time.time()
+    
+    train_loss, train_acc = train(model, train_iterator, optimizer, criterion)
+    valid_loss, valid_acc = evaluate(model, valid_iterator, criterion)
+    
+    end_time = time.time()
+
+    epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+    
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+        torch.save(model.state_dict(), 'Model-TextClassification-CNN.pt')
+    
+    print(f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
+    print(f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%')
+    print(f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%')
+
+
+# In[ ]:
+
+
+model.load_state_dict(torch.load('Model-TextClassification-CNN.pt'))
+
+test_loss, test_acc = evaluate(model, test_iterator, criterion)
+
+print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc*100:.2f}%')
+
+
+# In[ ]:
+
+
+tensor_dict = torch.load('Model-TextClassification-CNN.pt', map_location='cpu') # OrderedDict
+tensor_list = list(tensor_dict.items())
+for layer_tensor_name, tensor in tensor_list:
+    print('Layer {}: {} elements'.format(layer_tensor_name, torch.numel(tensor)))
+
+
+# In[ ]:
+
+
+import spacy
+nlp = spacy.load('en')
+
+def predict_toxicity(model, sentence, min_len = 5):
+    model.eval()
+    tokenized = [tok.text for tok in nlp.tokenizer(sentence)]
+    if len(tokenized) < min_len:
+        tokenized += ['<pad>'] * (min_len - len(tokenized))
+    indexed = [TEXT.vocab.stoi[t] for t in tokenized]
+    tensor = torch.LongTensor(indexed).to(device)
+    tensor = tensor.unsqueeze(0)
+    prediction = torch.sigmoid(model(tensor))
+    return prediction.item()
+
+
+# In[ ]:
+
+
+#predict_toxicity(model, "Test")
+
